33318251e97bdba0c036ac275ee4a8b5483ad313
tomavic <hbasheer@live.com>
2023-04-16 02:00:10 +0200

    style: pretty code

--- apps/home/src/app/app.module.ts
+++ apps/home/src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule } from "@angular/router";
-import { AppComponent } from "./app.component";
-import { appRoutes } from "./app.routes";
-import { NxWelcomeComponent } from "./nx-welcome.component";
-import { HeaderModule } from "@enigma/header";
-import { FooterModule } from "@enigma/footer";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { HeaderModule } from '@enigma/header';
+import { FooterModule } from '@enigma/footer';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -13,7 +13,7 @@ import { FooterModule } from "@enigma/footer";
     BrowserModule,
     HeaderModule,
     FooterModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: "enabledBlocking" }),
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
