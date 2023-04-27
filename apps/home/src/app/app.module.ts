81abf1d70352cd5bb73588758fce40ab600fdfce
tomavic <hbasheer@live.com>
2023-04-27 21:42:44 +0200

    feat(home): :sparkles: add primeng

--- apps/home/src/app/app.module.ts
+++ apps/home/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HeaderModule } from '@enigma-tech-lab/header';
 import { FooterModule } from '@enigma-tech-lab/footer';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HeaderModule,
     FooterModule,
+    ButtonModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
